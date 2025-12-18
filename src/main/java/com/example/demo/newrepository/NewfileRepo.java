e5837e0b5da32e65421ead0630ec104789d5c5f7
bala-amypo <bala@amypo.in>
2025-12-18 04:35:08 +0000

    User commit

--- src/main/java/com/example/demo/newrepository/NewfileRepo.java
+++ src/main/java/com/example/demo/newrepository/NewfileRepo.java
@@ -5,5 +5,5 @@ import org. springframework.data. jpa.repository. JpaRepository;
 import com.example.demo.newentity. NewfileEntity;
 
 public interface NewfileRepo extends JpaRepository<NewfileEntity, Long>{
-
+     
 }
